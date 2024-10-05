e24346cb687af86e57e1806c0b838052415ef006
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-10-05 07:27:42 +0600

    добавил типы, модель и роутер для юзера

---
+++ api/routers/users.ts
@@ -0,0 +1,89 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import User from '../models/User';
+
+const usersRouter = express.Router();
+
+usersRouter.get('/', async (_req, res, next) => {
+  try {
+    const users = await User.find();
+    return res.send(users);
+  } catch (e) {
+    next(e);
+  }
+});
+
+usersRouter.post('/', async (req, res, next) => {
+  try {
+    if (!req.body.username || !req.body.password) {
+      return res.status(400).send({ error: 'Username and password are required!' });
+    }
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+      displayName: req.body.displayName,
+    });
+
+    user.generateToken();
+    await user.save();
+
+    return res.send({ user });
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(e);
+    }
+
+    next(e);
+  }
+});
+
+usersRouter.post('/sessions', async (req, res, next) => {
+  try {
+    if (!req.body.username || !req.body.password) {
+      return res.status(400).send({ error: 'Username and password are required!' });
+    }
+    const user = await User.findOne({ username: req.body.username });
+
+    if (!user) {
+      return res.status(400).send({ error: 'Username or Password not found!' });
+    }
+
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if (!isMatch) {
+      return res.status(400).send({ error: 'Username or Password not found!' });
+    }
+
+    user.generateToken();
+    await user.save();
+
+    return res.send({ user });
+  } catch (e) {
+    next(e);
+  }
+});
+
+usersRouter.delete('/sessions', async (req, res, next) => {
+  try {
+    const headerValue = req.get('Authorization');
+
+    if (!headerValue) return res.status(204).send();
+
+    const [_bearer, token] = headerValue.split(' ');
+
+    if (!token) return res.status(204).send();
+
+    const user = await User.findOne({ token });
+
+    if (!user) return res.status(204).send();
+
+    user.generateToken();
+    await user.save();
+
+    return res.status(204).send();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+export default usersRouter;
---
+++ api/types.ts
@@ -0,0 +1,15 @@
+import {Model} from 'mongoose';
+
+export interface UserFields {
+  username: string;
+  password: string;
+  token: string;
+  displayName: string;
+}
+
+export interface UserMethods {
+  checkPassword(password: string): Promise<boolean>;
+  generateToken(): void;
+}
+
+export type UserModel = Model<UserFields, {}, UserMethods>;
